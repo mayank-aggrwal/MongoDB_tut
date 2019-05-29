73ce6b91f2837bbc38793bc62ce30a97c0ffff92
ronwon <aggarwal.mayank49@gmail.com>
2019-05-29 16:48:41 +0530

    Demo test mocha

---
+++ mongoTUT/test/demo_test.js
@@ -0,0 +1,9 @@
+
+const assert = require('assert');
+
+describe('Some demo test for the first time', () => {
+    it('adding two numbers', (done) => {
+        assert(2+3 === 5);
+        done();
+    })
+});
